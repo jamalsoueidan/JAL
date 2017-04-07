28e3cca08bbdab1c6c28a93309f3f76611458432
Jamal Soueidan <jse@kamstrup.com>
2017-04-07 10:23:49 +0200

    fix data not updating

--- src/components/table/content.js
+++ src/components/table/content.js
@@ -77,6 +77,10 @@ export default class Content extends React.Component {
   }
 
   componentWillUpdate(nextProps, nextState) {
+    if(nextProps.data!== this.props.data) {
+      this.data = this.props.data;
+    }
+    
     if(nextState.sort !== this.state.sort) {
       const sort = nextState.sort
       this.data = this.data.sort(sort);
